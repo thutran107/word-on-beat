c1dcda73e090dc1d127c2513786f69363c9a1518
Thu Tran <thutran@anduintransact.com>
2026-06-01 15:40:45 +0700

    fix: warmup countdown 1 bar instead of 2

--- word-on-beat/components/app.jsx
+++ word-on-beat/components/app.jsx
@@ -8,7 +8,7 @@ const TWEAK_DEFAULTS = /*EDITMODE-BEGIN*/{
   "bpmMedium": 120,
   "bpmHard": 150,
   "beatOffset": 3.25,
-  "warmupBars": 2,
+  "warmupBars": 1,
   "luxTitle": true
 }/*EDITMODE-END*/;
 
