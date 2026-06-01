9087e40298cf5d065f255e54531a862f064bf6a9
thutran107 <thutran@anduintransact.com>
2026-06-01 16:07:12 +0700

    Revert "feat: play waiting notes intro on each player's first level"

--- word-on-beat/components/app.jsx
+++ word-on-beat/components/app.jsx
@@ -8,7 +8,7 @@ const TWEAK_DEFAULTS = /*EDITMODE-BEGIN*/{
   "bpmMedium": 75,
   "bpmHard": 75,
   "beatOffset": 3.25,
-  "warmupBars": 1,
+  "warmupBars": 2,
   "luxTitle": true
 }/*EDITMODE-END*/;
 
